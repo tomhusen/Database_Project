5bd831154bc0695abbaeaf0a97aebdf02a6968ac
TEHUSEN <tehusen@linpe238vm6.ad.csbsju.edu>
2016-11-07 21:52:00 -0600

    Added some data insertions

--- Phase 3/TableCreation.sql
+++ Phase 3/TableCreation.sql
@@ -21,7 +21,8 @@ CREATE TABLE GABES_ADMIN(
   A_USERNAME VARCHAR(15) PRIMARY KEY,
   PASS VARCHAR(15) NOT NULL
   );
-  
+
+
 -- Create table for the Items - has all attributes (Check requirements for unique, etc.
 DROP TABLE GABES_ITEM CASCADE CONSTRAINTS;
 CREATE TABLE GABES_ITEM(
@@ -67,25 +68,43 @@ CREATE TABLE GABES_BIDS(
 -- ************************************************************
 -- ************************************************************
 
+-- Insertions for the GABES_USER table
 Insert Into GABES_USER VALUES
   (000001, 'tehusen', 'tehusen@csbsju.edu', 'p@$$word', '1234567890', 'Tom', 'Husen', 'tehusen', 1, 1);
+Insert Into GABES_USER VALUES
+  (000002, 'gkboyer', 'gkboyer@csbsju.edu', 'p@$$word1', '1234567890', 'Grant', 'Boyer', 'gkboyer', 1, 1);
+Insert Into GABES_USER VALUES
+  (000003, 'kaolson', 'kaolson@csbsju.edu', 'p@$$word2', '1234567890', 'Kyle', 'Olson', 'kaolson', 1, 1);
+Insert Into GABES_USER VALUES
+  (000004, 'irahal', 'irahal@csbsju.edu', 'p@$$word', '1234567890', 'Imad', 'Rahal', 'tehusen', 1, 1);
+  
+  
+  
+-- Insertions for the GABES_ADMIN table
+Insert Into GABES_ADMIN VALUES
+  ('tehusen', 'p@$$word');
+Insert Into GABES_ADMIN VALUES
+  ('gkboyer', 'p@$$word1');
+Insert Into GABES_ADMIN VALUES
+  ('kaolson', 'p@$$word2');
 
 
 
+-- Insertions for the GABES_ITEM table
+Insert Into GABES_ITEM VALUES
+  (0000000001, 'Books', 0, NULL, 'Book for Computer Science class', NULL, 'Database Systems', NULL, 15.00, to_date('2016-11-10','YYYY-MM-DD'), to_date('2016-12-02','YYYY-MM-DD'), 00001);
+Insert Into GABES_ITEM VALUES
+  (0000000002, 'Cooking', 0, NULL, 'Frying pan for cooking', NULL, 'Non-Stick Skillet', NULL, 17.99, to_date('2016-11-08','YYYY-MM-DD'), to_date('2016-12-01','YYYY-MM-DD'), 00001);
+Insert Into GABES_ITEM VALUES
+  (0000000003, 'Movies', 0, NULL, '2016 Disney-Pixar Film', NULL, 'Finding Dory', NULL, 19.99, to_date('2016-11-07','YYYY-MM-DD'), to_date('2016-11-24','YYYY-MM-DD'), 00001);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+-- Insertions for the GABES_FEEDBACK table
+Insert Into GABES_FEEDBACK VALUES
+  (0000000002, 4, 5, 4, 'Decent pan, does not cook evenly though.');
+Insert Into GABES_FEEDBACK VALUES
+  (0000000003, 9, 5, 8, 'Excellent movie - delivery was late though');
 
 
 
