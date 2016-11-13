7ebf2000c03070b51877d7e9566b850e97ddee01
tomhusen <husen.tom@gmail.com>
2016-11-13 13:41:11 -0600

    Added more data to sql table

--- Phase 3/TableCreation.sql
+++ Phase 3/TableCreation.sql
@@ -1,5 +1,5 @@
 -- *** GENERAL NOTES ***
--- For IS_ATTRIBUTE attributes (Seller, Buyer, Admin, etc)  and any other boolean value 
+-- For IS_ATTRIBUTE attributes (Seller, Buyer, Admin, etc)  and any other boolean value
 --    0 will denote false, and 1 will denote true.  These are CHAR(1) datatypes
 -- Dropped the Admin table for now - seems a lot simpler to have a boolean value
 --    for is_admin rather than a whole table just to have a single attribute
@@ -23,7 +23,7 @@ CREATE TABLE GABES_USER(
   A_USERNAME VARCHAR(15),
   IS_SELLER CHAR(1),
   IS_BUYER CHAR(1) --,
-  
+
  -- FOREIGN KEY (A_USERNAME) REFERENCES GABES_ADMIN(A_USERNAME)
   );
 
@@ -50,20 +50,20 @@ CREATE TABLE GABES_ITEM(
   START_DATE DATE NOT NULL,
   END_DATE DATE NOT NULL,
   USER_ID CHAR(6) NOT NULL,
-  
+
   FOREIGN KEY (USER_ID) REFERENCES GABES_USER(USER_ID)
   );
-  
+
 -- Create table for the Feedback - has all attributes
 DROP TABLE GABES_FEEDBACK CASCADE CONSTRAINTS;
 CREATE TABLE GABES_FEEDBACK(
-  ITEM_ID CHAR(6) PRIMARY KEY,
+  ITEM_ID CHAR(10) PRIMARY KEY,
   ITEM_QUALITY CHAR(1),                       -- Will be value 1-10
-  DELIVERY CHAR(1),                           -- Will be value 1-10
+  DELIVERY CHAR(2),                           -- Will be value 1-10
   OVERALL_RATING CHAR(1),                     -- Will be value 1-10
   COMMENTS VARCHAR(100)
   );
-  
+
 -- Create table for the Bids
 DROP TABLE GABES_BIDS CASCADE CONSTRAINTS;
 CREATE TABLE GABES_BIDS(
@@ -71,11 +71,11 @@ CREATE TABLE GABES_BIDS(
   USER_ID CHAR(6),
   BIDDING_TIME DECIMAL(15,15),                -- Probably need to change this data type
   MAX_BID DECIMAL(7, 2),
-  
+
   FOREIGN KEY (ITEM_ID) REFERENCES GABES_ITEM(ITEM_ID),
   FOREIGN KEY (USER_ID) REFERENCES GABES_USER(USER_ID)
   );
-  
+
 -- ************************************************************
 -- ************************************************************
 -- ************************************************************
@@ -99,9 +99,9 @@ Insert Into GABES_USER VALUES
   (000007, 'jrSmith', 'jrSmith@cavs.com', 'goCavs', '9521234567', 'JR', 'Smith', 0, 'gkboyer', 1, 1);
 Insert Into GABES_USER VALUES
   (000008, 'lJames', 'LeBron-J@yahoo.com', 'FlopAllDay', '6519842681', 'LeBron', 'James', 0, 'gkboyer', 1, 1);
-  
-  
-  
+
+
+
 ---- Insertions for the GABES_ADMIN table
 --Insert Into GABES_ADMIN VALUES
 --  ('tehusen', 'p@$$word');
@@ -117,37 +117,43 @@ Insert Into GABES_USER VALUES
 
 Insert Into GABES_ITEM VALUES
   (0000000001, 'Books', 0, NULL, 'Book for Computer Science class', NULL, 'Database Systems', NULL, 15.00, to_date('2016-12-10','YYYY-MM-DD'), to_date('2016-12-20','YYYY-MM-DD'), 00001);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000002, 'Cooking', 0, NULL, 'Frying pan for cooking', NULL, 'Non-Stick Skillet', NULL, 17.99, to_date('2016-12-08','YYYY-MM-DD'), to_date('2016-12-10','YYYY-MM-DD'), 00001);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000003, 'Movies', 0, NULL, '2016 Disney-Pixar Film', NULL, 'Finding Dory', NULL, 19.99, to_date('2016-12-07','YYYY-MM-DD'), to_date('2016-12-24','YYYY-MM-DD'), 00001);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000004, 'Books', 0, NULL, 'NY Times Bestseller!', NULL, 'The Da Vinci Code', NULL, 18.00, to_date('2016-12-15','YYYY-MM-DD'), to_date('2016-12-20','YYYY-MM-DD'), 00002);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000005, 'Books', 0, NULL, 'Biography by Walter Issacson', NULL, 'Steve Jobs', NULL, 21.99, to_date('2016-12-15','YYYY-MM-DD'), to_date('2016-12-19','YYYY-MM-DD'), 00006);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000006, 'Movies', 0, NULL, 'The second Avengers Movie', NULL, 'Avengers: Age of Ultron', NULL, 19.99, to_date('2016-12-07','YYYY-MM-DD'), to_date('2016-12-24','YYYY-MM-DD'), 00004);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000007, 'Electronics', 0, NULL, 'Also doubles as a fire starter', NULL, 'Samsung Note 7', NULL, 199.99, to_date('2016-12-10','YYYY-MM-DD'), to_date('2016-12-15','YYYY-MM-DD'), 00007);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000008, 'Merchandise', 0, NULL, 'Authentic Steph Curry Jersey', NULL, 'Curry #30 Jersey', NULL, 64.95, to_date('2016-12-08','YYYY-MM-DD'), to_date('2016-12-27','YYYY-MM-DD'), 00003);
-  
+
 Insert Into GABES_ITEM VALUES
   (0000000009, 'Clothing', 0, NULL, 'The North Face ultra-warm winter jacket', NULL, 'North Face Jacket', NULL, 149.99, to_date('2016-12-07','YYYY-MM-DD'), to_date('2016-12-22','YYYY-MM-DD'), 00004);
-  
+
 
 -- Insertions for the GABES_FEEDBACK table
+-- Feedback(Item ID, Item Quality, Delivery, Overall Rating, Comments)
 Insert Into GABES_FEEDBACK VALUES
   (0000000002, 4, 5, 4, 'Decent pan, does not cook evenly though.');
 Insert Into GABES_FEEDBACK VALUES
   (0000000003, 9, 5, 8, 'Excellent movie - delivery was late though');
-
-
-
+Insert Into GABES_FEEDBACK VALUES
+  (0000000007, 1, 1, 1, 'Sucks.  It started on fire. ');
+Insert Into GABES_FEEDBACK VALUES
+  (0000000009, 10, 10, 10, '');
+Insert Into GABES_FEEDBACK VALUES
+  (0000000006, 8, 3, 7, 'Pretty good movie - slow to deliver though');
+Insert Into GABES_FEEDBACK VALUES
+  (0000000007, 1, 8, 1, 'Super quick delivery - but the box was in flames');
